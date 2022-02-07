568579d51bfb89e214c143772afdf22842573005
Madison Henry <packback2000@users.noreply.github.com>
2022-02-07 11:52:48 -0500

    finalcommit

--- brainflix/src/components/Videos.js
+++ brainflix/src/components/Videos.js
@@ -19,10 +19,16 @@ class Videos extends React.Component {
         this.handleClick = this.handleClick.bind(this);
     }
 
-    handleClick = () => {
+    handleClick = (id) => {
+        console.log(id)
+        console.log(videodetails[this.state.currentVideo].id)
+        let videoIndex = this.state.videos.map(function(x) {return x.id;}).indexOf(id)
+        console.log(videoIndex)
+
         this.setState({
-            currentVideo: this.state.currentVideo + 1
+           currentVideo: videoIndex
         })
+
     }
 
     render() {
@@ -90,7 +96,7 @@ function Player(props) {
                 <ul key={props.currentVideoDetails.id} className="main-video__description">
                     <div className="main-video__description-box1">
                         <li className="main-video__description-channel">{props.currentVideoDetails.channel}</li>
-                        <li className="main-video__description-date">{fullDate}</li>
+                        <li className="main-video__description-date">date</li>
                     </div>
 
                     <div className="main-video__description-box2">
@@ -104,6 +110,61 @@ function Player(props) {
             </div>
             
             <div>
+                <p className="comments-number">{props.currentVideoDetails.comments.length} Comments</p>
+                <form className='comment-form'>
+            
+                        <input type="image" className='comment-form__image' src={Mohan} alt="text" />
+                        <div className='comment-form__input-container'>
+                        <label className="under480">JOIN THE CONVERSATION
+                        <input type="text" className='comment-form__input'placeholder='Add a new comment'/>
+                        </label>
+                        <button type='submit' className='comment-form__button' onClick={props.currentVideoDetails.changeVideo}>Comment</button>
+                        </div>
+                </form>
+             </div>
+
+             <p className="comments-comments">{props.currentVideoDetails.comments.map((comments) =>
+            <ul className="comments-list">
+            <hr />
+                <div className="comment-list-column1">
+                <li className="comments-list__avatar"></li>
+                </div>
+                
+                <div className="comments-list__line1">
+                <li className="comments-list__name">{comments.name}</li>
+                <li className="comments-list__timestamp">{comments.timestamp}</li>
+                </div>
+            
+            <div>
+                <li className="comments-list__comment">{comments.comment}</li>
+            </div>
+            
+            </ul>)}</p>
+            <hr />
+           
+        </section>
+    )
+}
+  
+function VideoList(props) {
+    
+    return (
+        <section className="next-videos" onClick={() => props.handleClick(props.id)} key={props.id} id={props.id}>
+            <img id={props.id} src={props.image} className="video-list__image" alt="a man looking left in front of a purple background"  />
+            <div className="video-list__container" id={props.id}>
+                <p className="video-list__title" id={props.id}>{props.title}</p>
+                <p className="video-list__channel" id={props.id}>{props.channel}</p>
+            </div>
+        </section>
+
+    )
+}
+
+/*
+
+ 
+
+     <div>
                 <p className="comments-number">{props.currentVideoDetails.comments.length} Comments</p>
                 <form className='comment-form'>
             
@@ -137,22 +198,6 @@ function Player(props) {
             
             </ul>)}</p>
             <hr />
-            
-        </section>
-    )
-}
-  
-function VideoList(props) {
-    
-    return (
-        <section className="next-videos" onClick={props.handleClick} key={props.id} id={props.id}>
-            <img id={props.id} src={props.image} className="video-list__image" alt="a man looking left in front of a purple background"  />
-            <div className="video-list__container" id={props.id}>
-                <p className="video-list__title" id={props.id}>{props.title}</p>
-                <p className="video-list__channel" id={props.id}>{props.channel}</p>
-            </div>
-        </section>
+              
 
-    )
-}
-
+    */
